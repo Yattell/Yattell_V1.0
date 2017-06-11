30552192a464f3b9d0f906ceb4d106953fc4686d
Cyferouss <tonyhao.wang@mail.utoronto.ca>
2017-06-11 18:43:04 -0400

    Updating uploads (image and comment)

---
+++ frontend_draft/yattell_theme_test/assets/js/uploads.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+
+    $("#btnSubmitImg").click(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        // Get form
+        var form = $('#imageUpload')[0];
+
+		// Create an FormData object
+        var data = new FormData(form);
+
+		// If you want to add an extra field for the FormData
+        data.append("CustomField", "This is some extra data, testing");
+
+		// disabled the submit button
+        $("#btnSubmitImg").prop("disabled", true);
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "http://174.138.59.72:3000/",
+            crossdomain: true,
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function (data) {
+
+                $("#result").text(data);
+                console.log("SUCCESS : ", data);
+                $("#btnSubmitImg").prop("disabled", false);
+
+            },
+            error: function (e) {
+
+                $("#result").text(e.responseText);
+                console.log("ERROR : ", e);
+                $("#btnSubmitImg").prop("disabled", false);
+
+            }
+        });
+
+    });
+
+    $("#btnSubmitDoc").click(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        // Get form
+        var form = $('#documentUpload')[0];
+
+    // Create an FormData object
+        var content = new FormData(form);
+
+    // If you want to add an extra field for the FormData
+        content.append("CustomField", "This is some extra data, testing");
+
+    // disabled the submit button
+        $("#btnSubmitDoc").prop("disabled", true);
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "http://174.138.59.72:3000/documents/",
+            crossdomain: true,
+            content: content,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function (data) {
+
+                $("#result").text(data);
+                console.log("SUCCESS : ", data);
+                $("#btnSubmitDoc").prop("disabled", false);
+
+            },
+            error: function (e) {
+
+                $("#result").text(e.responseText);
+                console.log("ERROR : ", e);
+                $("#btnSubmitDoc").prop("disabled", false);
+
+            }
+        });
+
+    });
+});
---
+++ frontend_draft/yattell_theme_test/test.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    $("#btnSubmit").click(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        // Get form
+        var form = $('#imageUpload')[0];
+
+		// Create an FormData object
+        var data = new FormData(form);
+
+		// If you want to add an extra field for the FormData
+        data.append("CustomField", "This is some extra data, testing");
+
+		// disabled the submit button
+        $("#btnSubmit").prop("disabled", true);
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "http://174.138.59.72:3000/",
+            crossdomain: true,
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function (data) {
+
+                $("#result").text(data);
+                console.log("SUCCESS : ", data);
+                $("#btnSubmit").prop("disabled", false);
+
+            },
+            error: function (e) {
+
+                $("#result").text(e.responseText);
+                console.log("ERROR : ", e);
+                $("#btnSubmit").prop("disabled", false);
+
+            }
+        });
+
+    });
+
+});
