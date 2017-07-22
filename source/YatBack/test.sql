7af04422c7d6e0ece28181a9efdbcd81136c0ac2
Yanbo Ding <yanboding@Yanbos-MacBook-Pro.local>
2017-07-22 18:38:25 +0800

    add source code for first version

---
+++ source/YatBack/test.sql
@@ -0,0 +1,69 @@
+INSERT INTO Article (title, summary, status, views, content, create_time, update_time, thumbnail, punchline, user_id) VALUES ('title1','summary1', 1,0,'content1',now(),now(),'thumbnail1','punchline1',1);
+INSERT INTO Article (title, summary, status, views, content, create_time, update_time, thumbnail, punchline,user_id) VALUES ('title2','summary2', 1,0,'content2',now(),now(),'thumbnail2','punchline2',1);
+INSERT INTO Article (title, summary, status, views, content, create_time, update_time, thumbnail, punchline,user_id) VALUES ('title3','summary3', 1,0,'content3',now(),now(),'thumbnail3','punchline3',1);
+INSERT INTO Article (title, summary, status, views, content, create_time, update_time, thumbnail, punchline,user_id) VALUES ('title4','summary4', 1,0,'content4',now(),now(),'thumbnail4','punchline4',1);
+INSERT INTO Article (title, summary, status, views, content, create_time, update_time, thumbnail, punchline,user_id) VALUES ('title5','summary5', 1,0,'content5',now(),now(),'thumbnail5','punchline5',1);
+INSERT INTO Article (title, summary, status, views, content, create_time, update_time, thumbnail, punchline,user_id) VALUES ('title6','summary6', 1,0,'content6',now(),now(),'thumbnail6','punchline6',1);
+
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (1,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (1,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (1,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (1,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (2,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (2,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (2,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (3,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (4,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (5,1,1,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (6,1,1,1,now(),now());
+
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (2,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (2,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (2,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (3,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (3,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (4,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (5,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (6,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,2,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,2,1,now(),now());
+
+
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (1,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (1,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (1,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (4,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (4,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (4,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (3,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (3,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (3,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (3,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,3,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,3,1,now(),now());
+
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (7,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (8,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (9,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (10,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (10,1,4,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (11,1,4,1,now(),now());
+
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (5,1,5,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (5,1,5,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (5,1,5,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (3,1,5,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (11,1,6,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (12,1,6,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (12,1,6,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (12,1,6,1,now(),now());
+INSERT INTO user_article_tag (tag_id, user_id, article_id, status, create_time, update_time) VALUES (13,1,6,1,now(),now());
+
+INSERT INTO comment (memory, content, commentable_type, commentable_id, status, create_time, update_time, user_id) VALUES (0, 'comment1', 'Article', 1, 1, now(), now(),1);
